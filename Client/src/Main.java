5fdcc85fe850c29e71e71513df6216bc76b6cfd2
Zdunek <zdunek369@gmail.com>
2019-05-25 22:08:09 +0200

    almost final one

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -6,15 +6,15 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
-    //CLIENT
 
-    int port = 4321;
-    String lHost = "127.0.0.1";
-
+    //  CLIENT  //
     public static void main(String[] args) {
         Main main = new Main();
         main.ServConnection();
     }
+
+    int port = 4321;
+    String lHost = "127.0.0.1";
 
     private void ServConnection() {
         try (Socket socket = new Socket(lHost, port)) {
@@ -34,13 +34,13 @@ public class Main {
                 // }
                 // break;
 
-            } while (!echoS.equals("exit"));
-            { //if echo form the server IS NOT EXIT, loop works
-                System.out.println("Connection stopped");
-            }
+            } while (!echoS.equals("exit")); //if echo form the server IS NOT EXIT, loop works
+            System.out.println("Connection stopped");
 
         } catch (IOException e) {
             System.out.println("Error - Client not connected: " + e.getMessage());
         }
     }
+
+
 }
--- Server/src/Main.java
+++ Server/src/Main.java
@@ -9,12 +9,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    //SERVER
+    //  SERVER  //
     public static void main(String[] args) {
-
         Main main = new Main();
         main.ClientServConnection();
     }
