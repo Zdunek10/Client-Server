db4b8b8deab2b626013a30354a78502588598496
Zdunek <zdunek369@gmail.com>
2019-05-25 19:38:33 +0200

    Warning servara przy odpowiedzi - odpowiedź do klienta jest porpawna.

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -7,21 +7,34 @@ import java.util.Scanner;
 
 public class Main {
 
+//CLIENT
+
     public static void main(String[] args){
-        try(Socket socket = new Socket("localhost" , 5000)) {
+        try(Socket socket = new Socket("127.0.0.1" , 5000)) {
 
             BufferedReader client = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+            PrintWriter stringToClient = new PrintWriter(socket.getOutputStream(), true);
 
             Scanner scanner = new Scanner(System.in);
-            String echoS;
-            String response;
+            String echoS, response;
 
 
+            do{
+                System.out.println("Please enter a mathematical equation for calculation: ");
+                echoS = scanner.nextLine(); //echoS = typed text into console
+                stringToClient.println(echoS);
+
+                if(!echoS.equals("exit")){
+                    response = client.readLine();
+                    System.out.println(response);
+                    System.out.println("Connection stopped");
+                }
+                break;
+            }while (!echoS.equals("exit")); //if echo form the server is not EXIT, loop works
 
         }
         catch (IOException e){
-            System.out.println("Error - Client not connected");
+            System.out.println("Error - Client not connected: " + e.getMessage());
         }
     }
 }
--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,6 +1,12 @@
+import sun.plugin.cache.OldCacheEntry;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,13 +17,45 @@ public class Main {
     public static void main(String[] args) {
 
         //server socket waits for request to come in over the network
+        //server listening on port 5000
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
-
 
             Socket socket = serverSocket.accept();
             System.out.println("Client connected");
 
+            //https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while (true){
+                String echoString = input.readLine();
+                //output.println("Echo from the server: " + echoString);
+
+               /* List<Character> chars = new ArrayList<>();
+                IntStream.range(0, echoString.length())
+                        .forEach(index -> chars.add(echoString.charAt(index)));
+                System.out.println(chars);*/
+
+                try {
+                    ScriptEngineManager mgr = new ScriptEngineManager();
+                    ScriptEngine engine = mgr.getEngineByName("JavaScript");
+//                    engine.eval(echoString);
+                    System.out.println("Wynik zapytania: " + engine.eval(echoString));
+                    output.println("The result of requested mathematic task '" +
+                            echoString + "' is: " +
+                            engine.eval(echoString));
+
+                } catch (ScriptException e){
+                    e.printStackTrace();
+                }
+
+
+                if(echoString.equals("exit")){ //if message sent from Client is EXIT, connection is stopped
+                    break;
+                }
+
+            }
+
 
 
         } catch (IOException e) {
@@ -25,4 +63,11 @@ public class Main {
         }
 
     }
+
+    private static void Obliczenie(String eString) {
+
+
+    }
+
 }
+
